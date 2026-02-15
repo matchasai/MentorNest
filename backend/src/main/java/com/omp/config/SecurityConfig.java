6caeb703a52f5c274199c59063f29c6d7270ce11
Sai Sujan <matchasai03@gmail.com>
2026-02-15 16:32:01 +0530

    fixed cors issue

--- backend/src/main/java/com/omp/config/SecurityConfig.java
+++ backend/src/main/java/com/omp/config/SecurityConfig.java
@@ -61,7 +61,10 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of(
+            "http://localhost:5173",
+            "https://mentornest.onrender.com"
+        ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true); // This is required for cookies
